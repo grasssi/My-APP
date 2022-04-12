0c893efcd80e23d4dd6fd9e74c8dc96678f1793a
GroSSo <grassi.amine@gmail.com>
2022-04-12 12:06:07 +0100

    Styling Elements Dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,11 +9,15 @@ export class ServerComponent implements OnInit {
 serverID:number=10
 serverStatus:string="offline"
 server:string="DHCP"
-  constructor() { }
+  constructor() {
+    this.serverStatus=Math.random() > 0.5 ? 'online' : 'offline'
+   }
 getServiceStatus(){
-  return this.serverID;
+  return this.serverStatus;
 }
   ngOnInit(): void {
   }
-
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red'
+  }
 }
